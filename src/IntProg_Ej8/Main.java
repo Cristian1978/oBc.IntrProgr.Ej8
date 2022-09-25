df8d00ebdd3eaa66e838e62803fa6592f25a76ae
Cristian1978 <cris.ar.sang@gmail.com>
2022-09-25 00:15:28 -0300

    commit 1

--- src/IntProg_Ej8/Main.java
+++ src/IntProg_Ej8/Main.java
@@ -1,2 +1,46 @@
-package IntProg_Ej8;public class Main {
+package IntProg_Ej8;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Crear objeto persona
+        Persona persona = new Persona();
+
+        persona.setNombre("Cristian");
+        System.out.println("Mi nombre es " + persona.getNombre());
+        persona.setEdad(43);
+        System.out.println("Tengo " + persona.getEdad() + " años de edad");
+        persona.setTelefono(379400000);
+        System.out.println("Mi teléfono es nro." + persona.getTelefono());
+    }
+}
+//Crear Clase Persona
+class Persona {
+    //Crear las variables: edad, nombre y teléfono de la clase Persona
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    // Gets y Sets de las variales
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
 }
